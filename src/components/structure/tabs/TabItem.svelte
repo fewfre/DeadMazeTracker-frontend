c3544ab52f1b0c6dbbfdd9bc75ffd7ae108cda7b
fewfre <wesitup@gmail.com>
2026-05-16 13:17:39 -0400

    suppressing state_referenced_locally warning in `TabItem`

--- src/components/structure/tabs/TabItem.svelte
+++ src/components/structure/tabs/TabItem.svelte
@@ -12,5 +12,6 @@
 	let { index, hash, titleSnippet, children }: Props = $props();
 
 	const tabsContext = getTabsContext();
+	// svelte-ignore state_referenced_locally - seems to work this way, and any changes just causes an endless loop
 	tabsContext.registerTab(index, titleSnippet, () => children, hash);
 </script>
