a973da637de89685433fd2d633cf65ca1cd1a733
fewfre <wesitup@gmail.com>
2026-05-27 14:14:40 -0400

    zone row bg images resized/cropped/translated, and css simplified

--- src/components/common/ZoneRowBackground.svelte
+++ src/components/common/ZoneRowBackground.svelte
@@ -3,15 +3,15 @@
 	
 	const background = [
 		null,
-		{ src:'/images/zone-row-backgrounds/zone-sac.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-bay.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-rosa.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-college.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-mall.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-walker.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-museum.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-highway.webp' },
-		{ src:'/images/zone-row-backgrounds/zone-mesa.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-1-sac.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-2-bay.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-3-rosa.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-4-college.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-5-mall.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-6-walker.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-7-museum.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-8-highway.webp' },
+		{ src:'/images/zone-row-backgrounds/zone-9-mesa.webp' },
 	][zoneId];
 </script>
 
@@ -24,13 +24,13 @@
 	position: absolute;
 	top: 0;
 	right: 0;
-	width: 25%;
+	width: 300px;
 	height: 100%;
 	
-	background-position: calc(100% + 200px) center;
+	background-position: right center;
 	background-repeat: no-repeat;
 	border-radius: var(--border-radius); /* parent uses it */
-	opacity: 0.5;
+	opacity: 0.55;
 	
 	mask-image: linear-gradient(to left, rgba(0,0,0,1), rgba(0,0,0,0));
 }
